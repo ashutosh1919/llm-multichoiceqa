24ed5f02f1485bd1b2ee213b5592e00d3494b3b0
Ashutosh Hathidara <ashutoshhathidara@gmail.com>
2023-07-16 22:57:27 +0000

    Adding utility code

---
+++ src/datastore.py
@@ -0,0 +1,80 @@
+import os
+from dataclasses import dataclass
+from datasets import Dataset
+from transformers import AutoTokenizer
+from transformers.tokenization_utils_base import PreTrainedTokenizerBase, PaddingStrategy
+from typing import Optional, Union
+import torch
+import pandas as pd
+
+
+class MultiChoiceDataStore:
+    def __init__(self,
+                 csv_file,
+                 dataset_type='train',
+                 id_col='id',
+                 question_col='prompt',
+                 answer_col='answer',
+                 num_options=5):
+        if not os.path.exists(csv_file):
+            raise ValueError('Unrecognized csv_file')
+        
+        self.ds = Dataset.from_pandas(pd.read_csv(csv_file))
+        assert dataset_type in ['train', 'test']
+        self.dataset_type = dataset_type
+        self.id_col = id_col
+        self.question_col = question_col
+        self.num_options = num_options
+        self.options = [chr(c) for c in list(range(ord('A'), ord('A') + num_options))]
+        
+        if dataset_type == 'train' and answer_col is None:
+            raise ValueError('answer_col cannot be None for dataset_type: train')
+        self.answer_col = answer_col
+        self.tokenizer = AutoTokenizer.from_pretrained("bert-base-uncased")
+        self.option_to_index = {opt: idx for idx, opt in enumerate(self.options)}
+        self.index_to_option = {idx: opt for idx, opt in enumerate(self.options)}
+        self.processed_ds = self.ds.map(self.preprocess,
+                                        batched=False,
+                                        remove_columns=[self.question_col] + self.options + [self.answer_col])
+
+    
+    def preprocess(self, example):
+        prompts = [example[self.question_col]] * self.num_options
+        choices = [example[opt_key] for opt_key in self.options]
+        # Our tokenizer will turn our text into token IDs BERT can understand
+        tokenized_example = self.tokenizer(prompts, choices, truncation=True)
+        tokenized_example['label'] = self.option_to_index[example[self.answer_col]]
+        return tokenized_example
+
+    
+    def __len__(self):
+        return len(self.processed_ds)
+
+
+@dataclass
+class DataCollatorForMultipleChoice:
+    tokenizer: PreTrainedTokenizerBase
+    padding: Union[bool, str, PaddingStrategy] = True
+    max_length: Optional[int] = None
+    pad_to_multiple_of: Optional[int] = None
+    
+    def __call__(self, features):
+        label_name = "label" if 'label' in features[0].keys() else 'labels'
+        labels = [feature.pop(label_name) for feature in features]
+        batch_size = len(features)
+        num_choices = len(features[0]['input_ids'])
+        flattened_features = [
+            [{k: v[i] for k, v in feature.items()} for i in range(num_choices)] for feature in features
+        ]
+        flattened_features = sum(flattened_features, [])
+        
+        batch = self.tokenizer.pad(
+            flattened_features,
+            padding=self.padding,
+            max_length=self.max_length,
+            pad_to_multiple_of=self.pad_to_multiple_of,
+            return_tensors='pt',
+        )
+        batch = {k: v.view(batch_size, num_choices, -1) for k, v in batch.items()}
+        batch['labels'] = torch.tensor(labels, dtype=torch.int64)
+        return batch
---
+++ src/trainer.py
@@ -0,0 +1,57 @@
+import argparse
+from transformers import AutoModelForMultipleChoice, TrainingArguments, Trainer
+import yaml
+from addict import Dict
+
+from datastore import MultiChoiceDataStore, DataCollatorForMultipleChoice
+
+
+def create_model(automodel_type):
+    return AutoModelForMultipleChoice.from_pretrained(automodel_type)
+
+def train(params):
+    """
+    Create and run model training pipeline
+    """
+    model = create_model(params.model.automodel_type)
+    train_ds = MultiChoiceDataStore(**params.dataset)
+    
+    training_args = TrainingArguments(
+        output_dir=params.model.model_dir,
+        evaluation_strategy=params.model.evaluation_strategy,
+        save_strategy=params.model.save_strategy,
+        load_best_model_at_end=params.model.load_best_model_at_end,
+        learning_rate=params.model.learning_rate,
+        per_device_train_batch_size=params.model.per_device_train_batch_size,
+        per_device_eval_batch_size=params.model.per_device_eval_batch_size,
+        num_train_epochs=params.model.epochs,
+        weight_decay=params.model.weight_decay,
+        report_to='none'
+    )
+    
+    trainer = Trainer(
+        model=model,
+        args=training_args,
+        train_dataset=train_ds.processed_ds,
+        eval_dataset=train_ds.processed_ds,
+        tokenizer=train_ds.tokenizer,
+        data_collator=DataCollatorForMultipleChoice(tokenizer=train_ds.tokenizer),
+    )
+    
+    trainer.train()
+
+
+def main():
+    parser = argparse.ArgumentParser(description='Training MultipleChoise QA dataset.')
+    parser.add_argument('--params_cfg', type=str, required=True)
+    args = parser.parse_args()
+    with open(args.params_cfg, 'r') as file:
+        params = yaml.safe_load(file)
+    params = Dict(params)
+    
+    print('Started training...')
+    train(params)
+
+
+if __name__ == '__main__':
+    main()
